1278839fe98abd9ab33e0dbb1c9e8d5c3510653f
Neeraj Kumar Yogi <nickyogi2143@gmail.com>
2025-07-13 22:44:54 +0530

    added pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./assets/Components/Store/Store";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      
+import App from "./App.jsx";
+import { store } from "./assets/Components/Store/Store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./index.css";
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      
-    </BrowserRouter>
-  </Provider>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-
+// ✅ Register service worker (only in production)
+if (import.meta.env.PROD) {
+  serviceWorkerRegistration.register();
+}
